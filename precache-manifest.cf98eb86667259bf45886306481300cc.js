a6a14359c946949b9995aa1210db2a6d5c797bf5
natt <mac@natts-MacBook-Pro.local>
2019-10-23 12:29:41 +0700

    Updates

---
+++ precache-manifest.cf98eb86667259bf45886306481300cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e064cdd6013b638b92d24b6f3e669276",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4c7fa98e2b8cdaad1fc3",
+    "url": "/monster-rolodex/static/css/main.b4658d57.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monster-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "4c7fa98e2b8cdaad1fc3",
+    "url": "/monster-rolodex/static/js/main.c71ca3ed.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
